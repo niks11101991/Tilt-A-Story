0ff5cd57c6ade7ce0cab09f64a8dbbb6b9032e36
ntalele <ntalele@ncsu.edu>
2015-05-05 19:59:14 -0400

    Uploaded the team project

---
+++ TILT-a-STORY/Assets/ScreenControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TILT-a-STORY/Assets/Scripts/TurnToLearnRotateScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnToLearnRotateScript : MonoBehaviour {
+	public int count;
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (count == 60) {
+			transform.Rotate (new Vector3 (0, 0, 30) * Time.deltaTime);
+			count=0;
+		} else {
+			transform.Rotate (new Vector3 (0, 0, -30) * Time.deltaTime);
+		}
+		count++;
+	}
+}
